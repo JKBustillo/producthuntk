1d8aaae3f39f1eb0ee4a50ec20a19ef5ec70d217
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-16 14:43:44 -0500

    feat: Trending page

---
+++ pages/trending.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+const Trending = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Trending</h1>
+      </Layout>
+    </div>
+  );
+}
+ 
+export default Trending;
