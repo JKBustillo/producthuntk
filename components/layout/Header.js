2706ffb5281c76f1e7a8cb69a95fb0d9c8191356
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-15 22:34:39 -0500

    feat: some styles in header, and HeaderContainer and Logo

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,13 +2,42 @@ import React from 'react';
 import Link from 'next/link';
 import Search from '../ui/Search';
 import Navigation from './Navigation';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const HeaderContainer = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--orange);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+    cursor: pointer;
+`;
 
 const Header = () => {
     return (
-        <header>
-            <div>
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gray3);
+                padding: 1rem 0;
+            `}
+        >
+            <HeaderContainer>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
 
                     <Search />
 
@@ -23,7 +52,7 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Register</Link>
                 </div>
-            </div>
+            </HeaderContainer>
         </header>
     );
 }
