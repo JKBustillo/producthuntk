f052b5bd9a9182977e960d0a571d8188cb90f3a5
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-16 14:19:45 -0500

    feat: InputText, ButtonSubmit and some css

--- components/ui/Search.js
+++ components/ui/Search.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const InputText = styled.input`
+    border: 1px solid var(--gray3);
+    padding: 1rem;
+    min-width: 300px;
+`;
+
+const ButtonSubmit = styled.button`
+    height: 3rem;
+    width: 3rem;
+    display: block;
+    background-size: 4rem;
+    background-image: url('/static/img/search.png');
+    background-repeat: no-repeat;
+    position: absolute;
+    right: 1rem;
+    top: 1px;
+    background-color: white;
+    border: none;
+    text-indent: -9999px;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
 
 const Search = () => {
     return (
-        <form>
-            <input type="text"/>
+        <form
+            css={css`
+                position: relative;
+            `}
+        >
+            <InputText
+                type="text"
+                placeholder="Search for anything"
+            />
 
-            <button type="submit">Buscar</button>
+            <ButtonSubmit type="submit">Buscar</ButtonSubmit>
         </form>
     );
 }
