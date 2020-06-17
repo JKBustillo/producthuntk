59fce460c830f2acd98433dfda9cf83655c96cdf
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-17 18:02:33 -0500

    feat: register form

--- pages/register.js
+++ pages/register.js
@@ -5,7 +5,39 @@ const Register = () => {
   return (
     <div>
       <Layout>
-        <h1>Create an account</h1>
+        <>
+          <h1>Create an account</h1>
+          <form>
+            <div>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Your nombre"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Your email"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Your password"
+              />
+            </div>
+            <input type="submit" value="Create account"/>
+          </form>
+        </>
       </Layout>
     </div>
   );
