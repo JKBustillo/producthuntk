7dc6c44b668c2313b5af4a759b522e7b5f610a6c
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-20 00:00:01 -0500

    feat: firebase config

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBX8SuAOJzx2RIDJpyUPtYn7Hf183PhGVA",
+    authDomain: "product-hunt-k.firebaseapp.com",
+    databaseURL: "https://product-hunt-k.firebaseio.com",
+    projectId: "product-hunt-k",
+    storageBucket: "product-hunt-k.appspot.com",
+    messagingSenderId: "835101383165",
+    appId: "1:835101383165:web:aab19a628854ea32236194"
+};
+
+export default firebaseConfig;
