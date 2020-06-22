b1471f44299ae25248816ca3e3027b2274541dfa
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-06-22 14:10:03 -0500

    feat: get products from the db

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('created', 'desc').onSnapshot(handleSnapshot);
+    };
+
+    getProducts();
+  }, []);
+
+  function handleSnapshot(snapshot) {
+    const products = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+
+    setProducts(products);
+  }
+
   return (
     <div>
       <Layout>
